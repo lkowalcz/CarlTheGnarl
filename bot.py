3f6a86134cbc1b537934cdbe8cf3911a2cb59787
lkowalcz <lkowalcz@gmail.com>
2012-10-25 01:42:06 -0400

    cleaning, plus profanity filter

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import twitter, os, re, random
+import twitter, os, sys, re, random
 import reply
 
 ####################################################
@@ -6,40 +6,49 @@ import reply
 # TODO: prune favorites after removing friends? (delete and favorited tweets of no longer friends)
 #
 ###################################################
-#ugly try catch all
+# main directory
+DIRECTORY = 'C:\Twitterbot\CarlTheGnarl'
+
+# log files
+LAST_MENTION_ID = 'log/last_id.txt'
+LAST_MESSAGE_ID = 'log/last_mid.txt'
+
+# authentication info
+CONSUMER_KEY='KrYmGq2yTOof2wt33nKNg'
+CONSUMER_SECRET='WAEDzQJAEIuiYWyjLxBERjdjgPFMCQpSFtlMBBIiRg'
+ACCESS_TOKEN_KEY='783117738-kb7DUZtCypv8DH8aq4g0AKiuQRiYK8xLMIBUYMaW'
+ACCESS_TOKEN_SECRET='g0pMGqMIFrYrW7cG5nzjw90NZHgKEH2QlkxQeaC7Ic'
+
+# chance of dropping an unfollowing friend
+PRUNE_PROB = 0.06
+# ADD_PROB * number of following = number of friends to add each cycle
+ADD_PROB = 0.04
+# chance of favoriting a new friend's status
+FAVORITE_PROB = 0.4
+# chance of following a person mentioned by a follower 
+BRANCH_PROB = 0.3
+# chance of replying to someone who has mentioned bot
+REPLY_PROB = 0.05
+
+# ski content that bot searches for to add friends
+SEARCH_TERMS = ['stoked ski', 'skiing powder', 'skiing', 'freeskiing', 'winter', 'big mountain skiing', 'park skiing', 'ski boots', 'ski mountain', 'newschoolers']
+# things bot likes to tweet about
+TWEET_TERMS = ['stoked ski', 'skiing powder', 'skiing', 'freeskiing', 'winter', 'big mountain skiing', 'park skiing', 'ski boots', 'ski mountain', 'newschoolers', 'san francisco', 'bay area', '49ers', 'wine', 'cooking', 'grilling', 'gym', 'workout', 'fantasy league', 'protein', 'golfing', 'baseball', 'wakeboarding', 'fishing', 'broing out', 'killin it', 'kayaking', 'mountain biking', 'cliff diving', 'basketball', 'paintball', 'kiteboarding', 'motocross', 'dirtbike', 'throwing down', 'rager', 'bench press', 'gnarly', 'urban skiing', 'jim wendler'] 
+# things carl doesn't like
+BLACKLIST = ['http', '@', 'shit', 'fuck', 'asshole', 'ass', 'bitch', 'nigga', 'nigger', 'motherfucker', 'fucker'] 
+
+followers = []
+friends = []
+
+##################
+# get stored info
+##################
 try:
-	# main directory
-	DIRECTORY = 'C:\Twitterbot\CarlTheGnarl'
-
-	# log files
-	LAST_MENTION_ID = 'log/last_id.txt'
-	LAST_MESSAGE_ID = 'log/last_mid.txt'
-
-	# authentication info
-	CONSUMER_KEY='KrYmGq2yTOof2wt33nKNg'
-	CONSUMER_SECRET='WAEDzQJAEIuiYWyjLxBERjdjgPFMCQpSFtlMBBIiRg'
-	ACCESS_TOKEN_KEY='783117738-kb7DUZtCypv8DH8aq4g0AKiuQRiYK8xLMIBUYMaW'
-	ACCESS_TOKEN_SECRET='g0pMGqMIFrYrW7cG5nzjw90NZHgKEH2QlkxQeaC7Ic'
-
-	# chance of dropping an unfollowing friend
-	PRUNE_PROB = 0.06
-	# ADD_PROB * number of following = number of friends to add each cycle
-	ADD_PROB = 0.04
-	# chance of favoriting a new friend's status
-	FAVORITE_PROB = 0.4
-	# chance of following a person mentioned by a follower 
-	BRANCH_PROB = 0.3
-	# chance of replying to someone who has mentioned bot
-	REPLY_PROB = 0.05
-
-	# ski content that bot searches for to add friends
-	SEARCH_TERMS = ['stoked ski', 'skiing powder', 'skiing', 'freeskiing', 'winter', 'big mountain skiing', 'park skiing', 'ski boots', 'ski mountain', 'newschoolers']
-	# things bot likes to tweet about
-	TWEET_TERMS = ['stoked ski', 'skiing powder', 'skiing', 'freeskiing', 'winter', 'big mountain skiing', 'park skiing', 'ski boots', 'ski mountain', 'newschoolers', 'san francisco', 'bay area', '49ers', 'wine', 'cooking', 'grilling', 'gym', 'workout', 'fantasy league', 'protein', 'golfing', 'baseball', 'wakeboarding', 'fishing', 'broing out', 'killin it', 'kayaking', 'mountain biking', 'cliff diving', 'basketball', 'paintball', 'kiteboarding', 'motocross', 'dirtbike', 'throwing down', 'rager', 'bench press', 'gnarly', 'urban skiing', 'jim wendler'] 
-
-	# get stored info
 	os.chdir(DIRECTORY)
+except:
+	print 'Error changing directory\n'
 
+try:
 	# get last status mention id used
 	if os.path.exists(LAST_MENTION_ID):
 		fp = open(LAST_MENTION_ID)
@@ -59,214 +68,222 @@ try:
 			last_message_id = 0
 	else:
 		last_message_id = 0
+except:
+	print 'Error reading log files\n'
 
-	# authenticate
+###############
+# authenticate
+###############
+try:
 	api = twitter.Api(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
+except:
+	print 'Error authenticating\n'
+	sys.exit()
 
-	# friend any followers that aren't friends
-	try:
-		print 'Friending any followers who are not friends...\n'
-		followers = api.GetFollowers()
-		friends = api.GetFriends()
-		for follower in followers:
-			notfollowing = 1
-			toFollow = follower.GetScreenName()
-			for friend in friends:
-				if friend.GetScreenName() == toFollow:
-					notfollowing = 0
-			if notfollowing:
-				print 'Following: ' + toFollow + '\n'
-				try:
-					api.CreateFriendship(toFollow)
-				except:
-					print 'Error making friend\n'
-	except:
-		print '\nError following people'
-
-	# prune friend list
-	print '\nPruning friend list...'
-	try:
-		followers = api.GetFollowers()
-		for friend in friends:
-			notfollowing = 1
-			toCheck = friend.GetScreenName()
-			for follower in followers:
-				if follower.GetScreenName() == toCheck:
-					notfollowing = 0
-			if notfollowing:
-				if random.random() < PRUNE_PROB:
-					print '\nRemoving: ' + toCheck
-					api.DestroyFriendship(toCheck)
-				else:
-					if len(api.GetFriends(toCheck)) < 5:
-						print '\nRemoving: ' + toCheck
-						api.DestroyFriendship(toCheck)
-	except:
-		print '\nError pruning friend list.'
-
-
-	# add new friends
-	friend_people = 1
-	if friend_people:
+###########################################
+# friend any followers that aren't friends
+###########################################
+try:
+	print 'Friending any followers who are not friends...\n'
+	followers = api.GetFollowers()
+	friends = api.GetFriends()
+	friendsNames = map((lambda friend: friend.GetScreenName()), friends)
+	newFollowers = filter((lambda follower: follower.GetScreenName() not in friendsNames), followers)
+	for follower in newFollowers:
+		print 'Following: ' + follower.GetScreenName() + '\n'
 		try:
-			toAdd = int(ADD_PROB * len(friends))
-			toSearch = random.choice(SEARCH_TERMS)
-			print '\nSearching for new friends who tweet about: ' + toSearch
-			candidates = api.GetSearch(toSearch)
-			for candidate in candidates:
-				if toAdd < 0:
-					break
-				name = candidate.GetUser().GetScreenName()
-				if name.lower() != 'carlthegnarl':
-					print '\nAdding: ' + name
-					try:
-						if random.random() < FAVORITE_PROB:
-							print '. Favoriting his tweet too!'
-							api.CreateFavorite(candidate)
-						api.CreateFriendship(name)
-						toAdd = toAdd - 1
-					except:
-						print '\nError adding ' + name
+			api.CreateFriendship(follower.GetScreenName())
 		except:
-			print '\nError looking for new friends'
+			print 'Error making friend\n'
+except:
+	print 'Error following followers\n'
 
-	# branch from current friends
-	branch = 1
-	if branch:
-		pattern = re.compile('@[^\s]*')
-		followers = api.GetFollowers()
-		friends = api.GetFriends()
-		toAdd = int(ADD_PROB * len(friends))
-		for follower in followers:
-			if toAdd < 0:
-				break
-			name = follower.GetScreenName()
+####################
+# prune friend list
+####################
+try:
+	print 'Pruning friend list...\n'
+	followersNames = map((lambda follower: follower.GetScreenName()), followers)
+	for friend in friends:
+		if friend.GetScreenName() not in followersNames:
+			# if friend is not following back, remove him with PRUNE_PROB probability
+			if random.random() < PRUNE_PROB:
+				print 'Removing: ' + friend.GetScreenName() + '\n'
+				try:
+					api.DestroyFriendship(friend.GetScreenName())
+				except:
+					print 'Error removing friend\n'
+			else:
+				# don't be friends with people who don't follow many people (they probably won't follow back)
+				if friend.friends_count < 10:
+					print 'Removing: ' + friend.GetScreenName() + '\n'
+					try:
+						api.DestroyFriendship(friend.GetScreenName())
+					except:
+						print 'Error removing friend\n'
+except:
+	print 'Error pruning friend list\n'
+
+##################
+# add new friends
+##################
+
+# using random keyword
+try:	
+	toAdd = int(ADD_PROB * len(friends))
+	toSearch = random.choice(SEARCH_TERMS)
+	print '\nSearching for new friends who tweet about: ' + toSearch
+	candidates = api.GetSearch(toSearch)
+	for candidate in candidates:
+		if toAdd < 0:
+			break
+		name = candidate.GetUser().GetScreenName()
+		if name.lower() != 'carlthegnarl':
+			print '\nAdding: ' + name
 			try:
-				statuses = api.GetUserTimeline(name)
-				for status in statuses:
-					text = status.GetText()
-					users = pattern.findall(text)
-					for user in users:
-						ustrip = user.strip()
-						if ustrip != '' and ustrip[1:].lower() != 'carlthegnarl':
-							tofriend = ustrip[1:];
-							if any(s.GetScreenName().lower() == tofriend.lower() for s in friends):
-								continue
-							else:
-								print '\nFollowing user: '+ tofriend
-								try:
-									if random.random() < BRANCH_PROB:
-										toAdd = toAdd - 1
-										api.CreateFriendship(tofriend)
-								except:
-									print 'error creating friendship with ' + tofriend
+				if random.random() < FAVORITE_PROB:
+					print '. Favoriting his tweet too!'
+					api.CreateFavorite(candidate)
+				api.CreateFriendship(name)
+				toAdd = toAdd - 1
 			except:
-				print 'Error accessing timeline for' + name
+				print '\nError adding ' + name
+except:
+	print '\nError looking for new friends'
 
-	# try to post something
-	tweet = 1
-	if tweet:
-		try:
-			print '\nLooking for status to tweet..'
-			candidates = api.GetSearch(random.choice(TWEET_TERMS))
-			for candidate in candidates:
-				try:
-					text = candidate.GetText()
-					# if a status doesn't mention anyone and doesn't have a link, use it
-					if text.find('@') == -1 and text.find('http') == -1:
-						# check if status is from a follower, if so, don't use it
-						candidate_name = candidate.GetUser().GetScreenName()
-						notfollower  = 1
-						for follower in followers:
-							if follower.GetScreenName() == candidate_name:
-								notfollower = 0
-						if notfollower:
-							# check if status has been used already
-							notrepeat = 1
-							mystatuses = api.GetUserTimeline()
-							for status in mystatuses:
-								if status.GetText().find(text) != -1:
-									notrepeat = 0
-							if notrepeat:
-								print '\nTweeting: ' + text
-								api.PostUpdate(text)
-								break
-				except:
-					print '\nError trying use a candidate tweet'
-		except:
-			print '\nError trying to post.'
-
-	# get direct messages since last id
-	checkmessages = 0
-	if checkmessages:
-		messages = api.GetDirectMessages(since_id=last_message_id)
-		for message in messages:
-			text = message.GetText()
-			print '\nGot message: ' + text
-			if random.random() < REPLY_PROB:
-				rep = reply.getreply(text)
-				if(rep != ''):
-					print '\nReplying: ' + rep
-					try:
-						api.PostDirectMessage(message.GetSenderId(), rep)
-					except:
-						print '\nError sending message!'
-				else:
-					print '\nNot replying'
-			else:
-				print '\nNot replying'
-		print '\nDone with messages'
-
-	# get mentions since last id
+# branching from current friends
+pattern = re.compile('@[^\s]*')
+toAdd = int(ADD_PROB * len(friends))
+# this is messy and needs to be cleaned up
+for follower in followers:
+	if toAdd < 0:
+		break
+	name = follower.GetScreenName()
 	try:
-		pattern = re.compile('@[^\s]*')
-		mentions = api.GetMentions(since_id=last_mention_id)
-		friends = api.GetFriends()
-		for status in mentions:
-			if random.random() < REPLY_PROB:
-				statusid = status.GetId()
-				poster = status.GetUser()
-				text = status.GetText()
-				print '\nFound status: ' + text
-				rep = reply.getreply(text)
-				if rep != '':
-					replying = '@' + poster.GetScreenName() + ' ' + rep
-					print '\nReplying: ' + replying
-					api.PostUpdate(replying, in_reply_to_status_id=statusid)
-				else:
-					print '\nNot replying..'
-				users = pattern.findall(text)
-				for user in users:
-					ustrip = user.strip()
-					if ustrip != '' and ustrip[1:].lower() != 'carlthegnarl':
-						tofriend = ustrip[1:];
-						if any(s.GetScreenName().lower() == tofriend.lower() for s in friends):
-							continue
-						else:
-							print '\nFollowing user: '+ tofriend
-							try:
+		statuses = api.GetUserTimeline(name)
+		for status in statuses:
+			text = status.GetText()
+			users = pattern.findall(text)
+			for user in users:
+				ustrip = user.strip()
+				if ustrip != '' and ustrip[1:].lower() != 'carlthegnarl':
+					tofriend = ustrip[1:];
+					if any(s.GetScreenName().lower() == tofriend.lower() for s in friends):
+						continue
+					else:
+						print 'Following user: '+ tofriend + '\n'
+						try:
+							if random.random() < BRANCH_PROB:
+								toAdd = toAdd - 1
 								api.CreateFriendship(tofriend)
-							except:
-								print 'error creating friendship with ' + tofriend
-			else:
-				api.CreateFavorite(status)
-
+						except:
+							print 'Error creating friendship with ' + tofriend + '\n'
 	except:
-		print '\nError processing mentions'
+		print 'Error accessing timeline for' + name
 
-	print '\nDone with mentions'
+###############
+# post a tweet
+###############
+try:
+	print 'Looking for status to tweet..\n'
+	candidates = api.GetSearch(random.choice(TWEET_TERMS))
+	for candidate in candidates:
+		try:
+			text = candidate.GetText()
+			# if a status doesn't mention anyone and doesn't have a link and doesn't use profanity, use it
+			if any(text.find(term) != -1 for term in BLACKLIST):
+				# check if status is from a follower, if so, don't use it
+				candidate_name = candidate.GetUser().GetScreenName()
+				if candidate_name not in followersNames:
+					# check if status has been used already
+					notrepeat = 1
+					mystatuses = api.GetUserTimeline()
+					for status in mystatuses:
+						if status.GetText().find(text) != -1:
+							notrepeat = 0
+					if notrepeat:
+						print 'Tweeting: ' + text + '\n'
+						api.PostUpdate(text)
+						break
+		except:
+			print 'Error trying use a candidate tweet\n'
+except:
+	print 'Error trying to post.\n'
 
-	# update logs
+####################################
+# get direct messages since last id
+####################################
+checkmessages = 0
+if checkmessages:
+	messages = api.GetDirectMessages(since_id=last_message_id)
+	for message in messages:
+		text = message.GetText()
+		print 'Got message: ' + text + '\n'
+		if random.random() < REPLY_PROB:
+			rep = reply.getreply(text)
+			if(rep != ''):
+				print 'Replying: ' + rep + '\n'
+				try:
+					api.PostDirectMessage(message.GetSenderId(), rep)
+				except:
+					print 'Error sending message!\n'
+			else:
+				print 'Not replying\n'
+		else:
+			print 'Not replying\n'
+	print 'Done with messages\n'
+
+#############################
+# get mentions since last id
+#############################
+try:
+	pattern = re.compile('@[^\s]*')
+	mentions = api.GetMentions(since_id=last_mention_id)
+	for status in mentions:
+		if random.random() < REPLY_PROB:
+			statusid = status.GetId()
+			poster = status.GetUser()
+			text = status.GetText()
+			print 'Found status: ' + text + '\n'
+			rep = reply.getreply(text)
+			if rep != '':
+				replying = '@' + poster.GetScreenName() + ' ' + rep
+				print 'Replying: ' + replying + '\n'
+				api.PostUpdate(replying, in_reply_to_status_id=statusid)
+			else:
+				print '\Not replying..\n'
+			users = pattern.findall(text)
+			for user in users:
+				ustrip = user.strip()
+				if ustrip != '' and ustrip[1:].lower() != 'carlthegnarl':
+					tofriend = ustrip[1:];
+					if any(s.GetScreenName().lower() == tofriend.lower() for s in friends):
+						continue
+					else:
+						print 'Following user: '+ tofriend + '\n'
+						try:
+							api.CreateFriendship(tofriend)
+						except:
+							print 'Error creating friendship with ' + tofriend + '\n'
+		else:
+			api.CreateFavorite(status)
+except:
+	print 'Error processing mentions\n'
+
+##############
+# update logs
+##############
+try:
 	if checkmessages:
 		if len([x.id for x in messages]) > 0 :
-			print '\nWriting new last_mid...'
+			print 'Writing new last_mid...\n'
 			fp = open(LAST_MESSAGE_ID, 'w')
 			fp.write(str(max([x.id for x in messages])))
 			fp.close()
 	if len([x.id for x in mentions]) > 0 :
-		print '\nWriting new last_id...'
+		print 'Writing new last_id...\n'
 		fp = open(LAST_MENTION_ID, 'w')
 		fp.write(str(max([x.id for x in mentions])))
 		fp.close()
 except:
-	print 'Error in script caught in ugly try-catch all'
+	print 'Error writing log files\n'
